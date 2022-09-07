01e4c0e2bcf14564321e12665c986c9a5381ae51
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-07 15:56:57 -0500

    Implementando búsqueda general en busquedasController.js

--- controllers/busquedasController.js
+++ controllers/busquedasController.js
@@ -1,12 +1,26 @@
 const { response } = require("express");
+const Hospital = require("../models/hospital");
+const Medico = require("../models/medico");
+const Usuario = require('../models/usuario');
 
-const getSearch = (req, res = response) => {
-    const busqueda = req.params.busqueda;    
+const getSearch = async(req, res = response) => {
+    const busqueda = req.params.busqueda;  
+    const regex = new RegExp( busqueda, 'i' );
+
+    const [ usuarios, medicos, hospitales ] = await Promise.all([
+        Usuario.find({ nombre: regex }),
+        Medico.find({ nombre: regex })
+                    .populate('usuario', 'nombre')
+                    .populate('hospital', 'nombre'),
+        Hospital.find({ nombre: regex })
+                      .populate('usuario', 'nombre email')
+    ]);
 
     res.status(200).json({ 
         ok: true,
-        msg: 'Get Busqueda',
-        busqueda
+        usuarios,
+        medicos,
+        hospitales
     });
 }
 
