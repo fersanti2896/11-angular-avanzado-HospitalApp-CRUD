021349a0f4f342b2b75bdcced26c18c0fb5dbf9e
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-07 15:59:46 -0500

    Añadiendo paginacion a los resultados de busqueda

--- controllers/busquedasController.js
+++ controllers/busquedasController.js
@@ -4,16 +4,23 @@ const Medico = require("../models/medico");
 const Usuario = require('../models/usuario');
 
 const getSearch = async(req, res = response) => {
+    const desde = Number( req.query.desde ) || 0;
     const busqueda = req.params.busqueda;  
     const regex = new RegExp( busqueda, 'i' );
 
     const [ usuarios, medicos, hospitales ] = await Promise.all([
-        Usuario.find({ nombre: regex }),
+        Usuario.find({ nombre: regex })
+               .skip( desde )
+               .limit( 5 ),
         Medico.find({ nombre: regex })
-                    .populate('usuario', 'nombre')
-                    .populate('hospital', 'nombre'),
+              .populate('usuario', 'nombre')
+              .populate('hospital', 'nombre')
+              .skip( desde )
+              .limit( 5 ),
         Hospital.find({ nombre: regex })
-                      .populate('usuario', 'nombre email')
+                .populate('usuario', 'nombre email')
+                .skip( desde )
+                .limit( 5 )
     ]);
 
     res.status(200).json({ 
