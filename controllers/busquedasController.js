f683d1ee55869b983ac0ca61a34d27103c2fc034
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-07 22:40:21 -0500

    Implementando búsqueda por colección específica en busquedaController.js

--- controllers/busquedasController.js
+++ controllers/busquedasController.js
@@ -31,6 +31,46 @@ const getSearch = async(req, res = response) => {
     });
 }
 
+const getDocumentsCollection = async(req, res = response) => {
+    const desde = Number( req.query.desde ) || 0;
+    const tabla = req.params.tabla;
+    const busqueda = req.params.busqueda;  
+    const regex = new RegExp( busqueda, 'i' );
+    let data = [];
+
+    switch (tabla) {
+        case 'hospitales': 
+            data = await Hospital.find({ nombre: regex })
+                                 .populate('usuario', 'nombre img')
+                                 .skip( desde )
+                                 .limit( 5 )
+            break;
+        case 'medicos': 
+            data = await Medico.find({ nombre: regex })
+                               .populate('usuario', 'nombre img')
+                               .populate('hospital', 'nombre img')
+                               .skip( desde )
+                               .limit( 5 )
+            break;
+        case 'usuarios': 
+            data = await Usuario.find({ nombre: regex })
+                                .skip( desde )
+                                .limit( 5 )
+            break;
+        default:
+            return res.status(400).json({
+                ok: false,
+                msg: 'La colección debe ser Médicos, Hospitales o Usuarios'
+            })
+    }
+
+    res.status(200).json({
+        ok: true,
+        resultados: data
+    });
+}
+
 module.exports = {
-    getSearch
+    getSearch,
+    getDocumentsCollection
 }
--- routes/busquedaRoutes.js
+++ routes/busquedaRoutes.js
@@ -5,12 +5,15 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 
-const { getSearch } = require('../controllers//busquedasController');
+const { getSearch, getDocumentsCollection } = require('../controllers//busquedasController');
 const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
 /* GET: Ruta | Controlador */
 router.get( '/:busqueda', validarJWT, getSearch );
+
+/* POST: Ruta | Middleware | Controlador */
+router.get ( '/coleccion/:tabla/:busqueda', validarJWT, getDocumentsCollection );
 
 module.exports = router;
